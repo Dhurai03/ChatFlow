584d0e10b0dd3f5695cedba76d0b46cbc41bee28
Dhurai Murugan B <149706515+Dhurai03@users.noreply.github.com>
2026-09-08 15:41:39 +0530

    Extend JWT token expiration from 7 days to 30 days

--- server/src/services/tokenService.js
+++ server/src/services/tokenService.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const signToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
 const verifyToken = (token) => {
